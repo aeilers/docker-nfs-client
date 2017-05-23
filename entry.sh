cd5d977cfac234bfd1bbb975991acd5a8e9304d4
Adam Eilers <adam.eilers@gmail.com>
2017-05-23 12:26:10 -0700

    testing a more secure NFS implementation with static ports

--- entry.sh
+++ entry.sh
@@ -1,15 +1,24 @@
 #! /bin/sh -e
 
+umount_nfs () {
+  echo "Unmounting nfs..."
+  umount "$MOUNTPOINT"
+  exit
+}
+
 mkdir -p "$MOUNTPOINT"
 chmod go+rw "$MOUNTPOINT"
 
 if [$SERVER == ""]; then
-rpc.statd & rpcbind -f & echo "docker NFS client with rpcbind ENABLED... if you wish to mount the mountpoint in this container USE THE FOLLOWING SYNTAX instead:
-$ docker run -itd --privileged=true --net=host -v vol:/mnt/nfs-1:shared -e SERVER= X.X.X.X -e SHARE=shared_path d3fk/nfs-client" & more
+    rpc.statd -F -p 32765 -o 32766 & rpcbind -f & echo "docker NFS client with rpcbind ENABLED... if you wish to mount the mountpoint in this container USE THE FOLLOWING SYNTAX instead:
+    $ docker run -itd --privileged=true --net=host -v vol:/mnt/nfs:shared -e SERVER= X.X.X.X -e SHARE=shared_path aeilers/docker-nfs-client" & more
 else
-rpc.statd & rpcbind -f &
-mount -t "$FSTYPE" -o "$MOUNT_OPTIONS" "$SERVER:$SHARE" "$MOUNTPOINT"
+    rpc.statd -F -p 32765 -o 32766 & rpcbind -f &
+    mount -t "$FSTYPE" -o "$MOUNT_OPTIONS" "$SERVER:$SHARE" "$MOUNTPOINT"
 fi
+
 mount | grep nfs
+
+trap umount_nfs SIGHUP SIGINT SIGTERM
 
 while true; do sleep 1000; done
